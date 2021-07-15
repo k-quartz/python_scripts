4b9fa693271d2d7bfae15b8e59efcbdfd7a02c4b
Quartz <quartzkhatri@gmail.com>
2021-07-15 15:01:35 +0530

    Update cmd_2_csv.py

--- cmd_2_csv.py
+++ cmd_2_csv.py
@@ -1,18 +1,17 @@
-import subprocess
 import os
+import subprocess
 
-batcmd="dir"
+batcmd = "dir"
 
-result=subprocess.check_output(batcmd,shell=True).decode("utf-8")
+# Below line will get the out put of the command in result variable
+result = subprocess.check_output(batcmd, shell=True).decode("utf-8")
 
-result=result.split("\n")
-
-
+# below lines are just for "dir" command it will not be same for all the commands.
+result = result.split("\n")
 for each in result[8:-3]:
-    filname=each.split()[-1]
+    filname = each.split()[-1]
     print(filname)
 
-f=open("sample.csv","a+")
+f = open("sample.csv", "a+")
 f.write(str(result))
 f.close()
-
